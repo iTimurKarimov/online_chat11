097d70a69f07b4b97d3d28f8b9405de5d6bef3c5
iTimurKarimov <134382153+iTimurKarimov@users.noreply.github.com>
2023-06-08 19:36:31 +0300

    Update test.py

--- test.py
+++ test.py
@@ -78,5 +78,31 @@ with st.sidebar:
             st.write("Введите имя пользователя и пароль")
 
 while True:
+    if username and password:
+        messages_container = st.empty()
+
+        def display_messages():
+
+            if not os.path.exists("messages.txt"):
+                with open("messages.txt", "w") as f:
+                    f.write("")
+
+            with open("messages.txt", "r", encoding='utf-8') as f:
+                messages = f.readlines()
+                messages_container.empty()
+                message = '\n'.join(messages)
+
+                messages_container.write(f"<div class='message'><b>:</b> {message}</div>", unsafe_allow_html=True)
+
+        message = st.text_input("Введите сообщение", max_chars=500, key="message_input")
+        if st.button("Отправить"):
+            if message:
+                with open("messages.txt", "a+") as f:
+                    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    f.write(f"{username}: {message} ({now})\n")
+            else:
+                st.write("Введите сообщение")
+
+        display_messages()
 
     time.sleep(1)
